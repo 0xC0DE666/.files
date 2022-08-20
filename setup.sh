8e8cfc948b9349f59d9b900bb6fff747f4809156
PatMan10 <PatMan10@protonmail.com>
2022-08-20 06:51:19 +0200

    pulling certain vim scripts from github

--- setup.sh
+++ setup.sh
@@ -4,15 +4,29 @@ echo "dowloading neccesary resources..."
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 echo "setting up variables...";
-dot_dir=`pwd`;
+wd=`pwd`;
 
-echo "creating home configs...";
-cp $dot_dir/.gitconfig $HOME/.gitconfig;
-cp $dot_dir/.zshrc $HOME/.zshrc;
+echo "creating ~ configs...";
+cp $wd/.gitconfig $HOME/.gitconfig;
+cp $wd/.zshrc $HOME/.zshrc;
 
-echo "creating home/.config configs...";
-[ ! -d $HOME/.config ] && mkdir $HOME/.config || : ;
+echo "creating ~/.config configs...";
+config_dir=$HOME/.config;
+[ ! -d $config_dir ] && mkdir $config_dir || : ;
+cp -R $wd/.config/* $config_dir;
 
-cp -R $dot_dir/.config/nvim $HOME/.config/nvim;
+#////////////
+# setup nvim
+#////////////
+
+# vim plugged
+autoload_dir=$config_dir/nvim/autoload
+[ ! -d  $autoload_dir ] && mkdir $autoload_dir || : ;
+curl -fsSL https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim > $autoload_dir/plug.vim
+
+# spacegray color scheme
+colors_dir=$config_dir/nvim/colors
+[ ! -d $colors_dir ] && mkdir $colors_dir || : ;
+curl -fsSL https://raw.githubusercontent.com/ackyshake/Spacegray.vim/main/colors/spacegray.vim > $colors_dir/spacegray.vim
 
 echo "-- done --"
