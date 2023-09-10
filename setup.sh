1154a84d7eb8697837b0ec4e3671593aa60b0966
PatMan10 <PatMan10@protonmail.com>
2023-09-10 08:44:35 +0200

    removed spacegray.vim to seperate gist

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "dowloading neccesary resources..."
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo "dowloading neccesary resources...";
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
 
 echo "setting up variables...";
 wd=`pwd`;
@@ -15,4 +15,8 @@ config_dir=$HOME/.config;
 [ ! -d $config_dir ] && mkdir $config_dir || : ;
 cp -R $wd/.config/* $config_dir;
 
-echo "-- done --"
+colors_dir=$config_dir/nvim/colors;
+mkdir colors_dir;
+curl https://gist.githubusercontent.com/PatMan10/f743f664a5ac7815d38a2c8141a1aaf7/raw/e23f7383fec99ae6c13361e90cdeb0131fa33903/spacegray.vim > $colors_dir/spacegray.vim;
+
+echo "-- done --";
