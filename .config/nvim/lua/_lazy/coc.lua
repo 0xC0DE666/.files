1436ced08ffb4e10474bbb56b07714785456deb9
PatMan10 <PatMan10@protonmail.com>
2024-05-28 11:45:45 +0200

    removed conflicting leader key binds

--- .config/nvim/lua/_lazy/coc.lua
+++ .config/nvim/lua/_lazy/coc.lua
@@ -59,12 +59,12 @@ vim.api.nvim_create_autocmd("CursorHold", {
 
 
 -- Symbol renaming
-keyset("n", "<leader>rn", "<Plug>(coc-rename)", {silent = true})
+keyset("n", "<backslash>rn", "<Plug>(coc-rename)", {silent = true})
 
 
 -- Formatting selected code
-keyset("x", "<leader>f", "<Plug>(coc-format-selected)", {silent = true})
-keyset("n", "<leader>f", "<Plug>(coc-format-selected)", {silent = true})
+keyset("x", "<backslash>f", "<Plug>(coc-format-selected)", {silent = true})
+keyset("n", "<backslash>f", "<Plug>(coc-format-selected)", {silent = true})
 
 
 -- Setup formatexpr specified filetype(s)
@@ -84,25 +84,25 @@ vim.api.nvim_create_autocmd("User", {
 })
 
 -- Apply codeAction to the selected region
--- Example: `<leader>aap` for current paragraph
+-- Example: `<backslash>aap` for current paragraph
 local opts = {silent = true, nowait = true}
-keyset("x", "<leader>a", "<Plug>(coc-codeaction-selected)", opts)
-keyset("n", "<leader>a", "<Plug>(coc-codeaction-selected)", opts)
+keyset("x", "<backslash>a", "<Plug>(coc-codeaction-selected)", opts)
+keyset("n", "<backslash>a", "<Plug>(coc-codeaction-selected)", opts)
 
 -- Remap keys for apply code actions at the cursor position.
-keyset("n", "<leader>ac", "<Plug>(coc-codeaction-cursor)", opts)
+keyset("n", "<backslash>ac", "<Plug>(coc-codeaction-cursor)", opts)
 -- Remap keys for apply source code actions for current file.
-keyset("n", "<leader>as", "<Plug>(coc-codeaction-source)", opts)
+keyset("n", "<backslash>as", "<Plug>(coc-codeaction-source)", opts)
 -- Apply the most preferred quickfix action on the current line.
-keyset("n", "<leader>qf", "<Plug>(coc-fix-current)", opts)
+keyset("n", "<backslash>qf", "<Plug>(coc-fix-current)", opts)
 
 -- Remap keys for apply refactor code actions.
-keyset("n", "<leader>re", "<Plug>(coc-codeaction-refactor)", { silent = true })
-keyset("x", "<leader>r", "<Plug>(coc-codeaction-refactor-selected)", { silent = true })
-keyset("n", "<leader>r", "<Plug>(coc-codeaction-refactor-selected)", { silent = true })
+keyset("n", "<backslash>re", "<Plug>(coc-codeaction-refactor)", { silent = true })
+keyset("x", "<backslash>r", "<Plug>(coc-codeaction-refactor-selected)", { silent = true })
+keyset("n", "<backslash>r", "<Plug>(coc-codeaction-refactor-selected)", { silent = true })
 
 -- Run the Code Lens actions on the current line
-keyset("n", "<leader>cl", "<Plug>(coc-codelens-action)", opts)
+keyset("n", "<backslash>cl", "<Plug>(coc-codelens-action)", opts)
 
 
 -- Map function and class text objects
--- .config/nvim/lua/_lazy/harpoon.lua
+++ .config/nvim/lua/_lazy/harpoon.lua
@@ -1,7 +1,7 @@
 local mark = require("harpoon.mark");
 local ui = require("harpoon.ui");
 
-vim.keymap.set('n', "<leader>h", mark.add_file);
+vim.keymap.set('n', "<leader>p", mark.add_file);
 vim.keymap.set('n', "<leader>m", ui.toggle_quick_menu);
 
 vim.keymap.set('n', "<leader>1", function() ui.nav_file(1) end);
