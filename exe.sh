aeebbedd9deaab3dd465d303bacbd95c3a378ce7
0xC0DE666 <0xC0DE666@proton.me>
2024-10-06 15:31:20 +0200

    succesfully over engineerd setup script

---
+++ exe.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+help() {
+  printf "Usage\n"
+  printf "    exe.sh --opt\n\n"
+  printf "    Run \"exe.sh --options\" to get the list of possible options.\n\n"
+}
+
+options() {
+  echo "-h, --help     -  Print help."
+  echo "-o, --options  -  Print options."
+  echo "-s, --setup    -  Setup dot files."
+  echo "-c, --cleanup  -  Cleanup dot files."
+}
+
+setup() {
+  echo "copying ~ config files...";
+  w_dir=`pwd`;
+  cp -R $w_dir/bin $HOME/bin;
+  cp $w_dir/.zshrc $HOME/.zshrc;
+  cp $w_dir/.gitconfig $HOME/.gitconfig;
+  cp $w_dir/.tmux.conf $HOME/.tmux.conf;
+  
+  echo "copying ~/.config/ files...";
+  config_dir=$HOME/.config;
+  [ ! -d $config_dir ] && mkdir $config_dir || : ;
+  cp -R $w_dir/.config/* $config_dir;
+  
+  colors_dir=$config_dir/nvim/colors;
+  mkdir $colors_dir;
+  curl --silent https://gist.githubusercontent.com/PatMan10/f743f664a5ac7815d38a2c8141a1aaf7/raw/e23f7383fec99ae6c13361e90cdeb0131fa33903/spacegray.vim > $colors_dir/spacegray.vim;
+   echo "setting up ohmyzsh...";
+  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
+ 
+  echo "-- done --";
+}
+
+cleanup() {
+  echo "cleaning dot files..."
+  rm -rf ~/bin ~/.ohmyzsh ~/.config/i3 ~/.config/nvim ~/.config/terminator ~/.config/coc;  
+  rm ~/.zshrc ~/.zsh_history ~/.gitconfig ~/.tmux.conf;  
+  echo "-- done --";
+}
+
+if [ $# != 1 ]; then
+  help;
+  exit 1;
+else
+  case $1 in
+    "-h")
+      help;;
+    "--help")
+      help;;
+    "-o")
+      options;;
+    "--options")
+      options;;
+    "-s")
+      setup;;
+    "--setup")
+      setup;;
+    "-c")
+      cleanup;;
+    "--cleanup")
+      cleanup;;
+    *)
+       printf "Invalid option.\nRun \"bcs --options\" to get a list of options.\n";
+       exit 1;;
+    esac
+fi
